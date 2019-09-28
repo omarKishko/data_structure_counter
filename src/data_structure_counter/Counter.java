0e1d56c94a149c274d3116dc4b1f59724fa3167d
omar kishko <omarskishko@gmail.com>
2019-09-29 01:20:03 +0300

    initial commit

---
+++ src/data_structure_counter/Counter.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data_structure_counter;
+
+/**
+ *
+ * @author omar kishko
+ */
+public class Counter {
+
+    public Counter()
+    {
+        // ADD CODE FOR THE CONSTRUCTOR
+    }
+
+    public Counter(int min, int max)
+    {
+        // ADD CODE FOR THE ALTERNATE CONSTRUCTOR
+    }
+    
+    public boolean equals(Object otherObject)
+    {
+        boolean result = true;
+        return result;
+    }
+    
+
+    public void increase()
+    {
+        // ADD CODE TO INCREASE THE VALUE OF THE COUNTER
+    }
+ 
+
+    public void decrease()
+    {
+        // ADD CODE TO INCREASE THE VALUE OF THE COUNTER
+    }
+    
+
+    public int value()
+    {
+        // CHANGE THE RETURN TO GIVE THE CURRENT VALUE OF THE COUNTER
+        return -50;
+		
+    }
+
+    public boolean rolledOver()
+    {
+        // CHANGE THE RETURN TO THE ROLLOVER STATUS OF THE COUNTER
+        return true;
+    }
+
+    public String toString()
+    {
+        // CHANGE THE RETURN TO A DESCRIPTION OF THE COUNTER
+        return "";		
+    }
+}
